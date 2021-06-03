1b51e7efe95a3fd0c0b951808345b03efa977fa8
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-06-03 14:23:17 +0530

    Refactored : To use while loop to satisfy the given condition of empHrs and Days

--- calculatingEmployeeWage.js
+++ calculatingEmployeeWage.js
@@ -1,11 +1,13 @@
-//UC 3 Refactor to write a function
+//UC3 Refactor to write a function
 //UC4 Calculate wages for a month
+//UC5 Refactor to write a while loop
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -22,10 +24,13 @@ function getWorkingHours(empCheck) {
 }
 
 let totalEmpHrs = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+let totalWorkingDays = 0;
+while ((totalEmpHrs <= MAX_HRS_IN_MONTH) && (totalWorkingDays < NUM_OF_WORKING_DAYS)) {
     let empCheck = Math.floor(Math.random() * 10) % 3;
     totalEmpHrs += getWorkingHours(empCheck);
+    totalWorkingDays++;
 }
 
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total working days: " + totalWorkingDays);
 console.log("Total emp hours = " + totalEmpHrs + " Total employee wage = " + empWage);
